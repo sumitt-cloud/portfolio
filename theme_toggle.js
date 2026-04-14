23e4f2312295b987f412bcb2f23e9e5259efaac9
sumitt-cloud <sumit.leonova1615@gmail.com>
2026-04-14 18:14:38 +0530

    adding icon changing logic

--- theme_toggle.js
+++ theme_toggle.js
@@ -1,8 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("themeBtn")
+  const icon = btn.querySelector("i")
   const html = document.documentElement
 
   btn.addEventListener("click", () => {
     html.classList.toggle("dark")
+
+    if(html.classList.contains("dark")){
+      icon.classList.remove("fa-sun")
+      icon.classList.add("fa-moon")
+    }
+    else{
+      icon.classList.remove("fa-moon")
+      icon.classList.add("fa-sun")
+    }
   })
-})
